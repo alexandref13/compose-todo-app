be8274cd3f129a8bc22e01d926ab93d14fce07ee
Alexandre Fernandes <alefernandeseng@gmail.com>
2024-01-19 19:32:11 -0300

    style: change colors in Theme

--- app/src/main/java/br/com/ale/composefirstapp/ui/theme/Color.kt
+++ app/src/main/java/br/com/ale/composefirstapp/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package br.com.ale.composefirstapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primaryLight = Color(0xFF8257E5)
+val successLight = Color(0xFF1DB863)
+val whiteColor = Color(0xFFFFFFFF)
+val grayColor = Color(0xFFB2B2B2)
+val gray2Color = Color(0xFF525252)
